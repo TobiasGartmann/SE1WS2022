4bb4a6806a6d38bea69895a0d41b47d4cb3d4544
TobiasGartmann <CrimbelHD@gmail.com>
2022-10-23 01:54:20 +0200

    Updates

--- src/org/hbrs/s1/ws22/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/s1/ws22/uebung1/control/GermanTranslator.java
@@ -1,16 +1,30 @@
 package org.hbrs.s1.ws22.uebung1.control;
 
+import java.util.HashMap;
+
 public class GermanTranslator implements Translator {
 
 	public String date = "Okt/2022"; // Default-Wert
+	private static HashMap<Integer, String> map = new HashMap<Integer, String>();
+	static  {
+		map.put(1, "eins");
+		map.put(2, "zwei");
+		map.put(3, "drei");
+		map.put(4, "vier");
+		map.put(5, "fünf");
+		map.put(6, "sechs");
+		map.put(7, "sieben");
+		map.put(8, "acht");
+		map.put(9, "neun");
+		map.put(10, "zehn");
+	}
 
 	/**
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
 	 */
 	public String translateNumber( int number ) {
 		// [ihr Source Code aus Übung 1-2]
-
-		return null;
+		return map.getOrDefault(number, "Übersetzung der Zahl "+ number +" nicht möglich ("+version+")");
 	}
 
 	/**
