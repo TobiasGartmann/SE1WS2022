2a3f102531bd6ece82c47177ce357dfea16b22f1
TobiasGartmann <CrimbelHD@gmail.com>
2022-10-23 23:53:34 +0200

    Implementation of Client

---
+++ src/org/hbrs/se1/ws22/uebung3/Client.java
@@ -0,0 +1,24 @@
+package org.hbrs.se1.ws22.uebung3;
+
+public class Client {
+    public void viewContainer() {
+        Member m1 = new ConcreteMember(1);
+        Member m2 = new ConcreteMember(2);
+        Member m3 = new ConcreteMember(3);
+        Member m4 = new ConcreteMember(4);
+
+        Container con = Container.getInstance();
+
+        try {
+            con.addMember(m1);
+            con.addMember(m2);
+            con.addMember(m3);
+            con.addMember(m4);
+        } catch (ContainerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        MemberView memView = new MemberView();
+        memView.dump(con.getCurrentList());
+    }
+}
